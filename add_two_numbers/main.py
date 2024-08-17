ff51f85c365c472cb6d097ea5549c7baca78a6b9
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-17 23:07:27 +0200

    feature: added better solution based on LinkNodes

--- add_two_numbers/main.py
+++ add_two_numbers/main.py
@@ -14,6 +14,34 @@ Output: 0 -> 0
 Explanation: 56 + 56 = 112.
 """
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def addTwoNumbers_better(l1: ListNode, l2: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
 def add_two_numbers(l1: list, l2: list) -> list:
     """
     Add two number to each other
@@ -48,4 +76,15 @@ l2 = [5,6,4]
 add_two_numbers(l1,l2)
 l1 = [9,9,9,9,9,9,9]
 l2 = [9,9,9,9]
-add_two_numbers(l1,l2) #Output: [8,9,9,9,0,0,0,1]
+add_two_numbers(l1,l2) #Output: [8,9,9,9,0,0,0,1] # TODO: fix this
+
+l1 = ListNode(2)
+l2 = ListNode(4)
+l3 = ListNode(3)
+l4 = ListNode(5)
+l5 = ListNode(6)
+l6 = ListNode(4)
+
+addTwoNumbers_better(l1, l2)
+addTwoNumbers_better(l3, l5)
+
