dbd271159978b9ff7035291879b6f5691e1c0329
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-23 14:05:29 +0200

    feature: fixed version of zigzag

--- zigzag_conversation/main.py
+++ zigzag_conversation/main.py
@@ -5,7 +5,7 @@ Example:
 
 P   A   H   N
 A P L S I I G
-Y   I   E   .
+Y   I   I   .
 
 And then read line by line: "PAHNAPLSIIGYEI"
 
@@ -17,26 +17,25 @@ Input: "PAYPALISHIRING"
 Output: "PAHNAPLSIIGYEI"
 """
 def zigzag_conversation(s: str, rows: int) -> str:
-    list_of_string = list(s)
-    skip = False
-    counter = 0
-    for i in range(len(list_of_string)):
-        if skip:
-            skip = False
-            continue
-        element = list_of_string[i]
-        print(element)
+    if len(s) == 0:
+        return ""
+    if rows == 1:
+        return s
+    output_str = ""
+    for i in range(rows):
+        inc = 2 * (rows - 1)
+        for j in range(i, len(s), inc):
+            output_str += s[j]
+            if i > 0 and i < rows - 1 and j + inc - 2 * i < len(s):
+                output_str += s[j + inc - 2 * i]
+    return output_str
 
-        counter += 1
-        if counter % rows == 0:
-            print()
-            print(list_of_string[i+1])
-            print()
-            counter = 0
-            skip = True
+
 
 
 def test_function(f):
-    assert f("PAYPALISHIRING", 3) == "PAHNAPLSIIGYEI", "Test case 1 failed"
+    assert f("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR", "Test case 1 failed"
+    assert f("PAYPALISHIRING", 4) == "PINALSIGYAHRPI", "Test case 2 failed"
+    assert f("A", 1) == "A", "Test case 3 failed"
 
 test_function(zigzag_conversation)
