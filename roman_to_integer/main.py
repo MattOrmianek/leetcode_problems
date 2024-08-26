ed1ed174e8b60860353eb8059f898c0cf48e1ba2
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-26 18:33:27 +0200

    feature: roman to integer done

--- roman_to_integer/main.py
+++ roman_to_integer/main.py
@@ -36,3 +36,23 @@ Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 """
+
+
+def roman_to_int(s):
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+
+    for i in range(len(s)):
+        if i > 0 and roman_dict[s[i]] > roman_dict[s[i - 1]]:
+            result += roman_dict[s[i]] - 2 * roman_dict[s[i - 1]]
+        else:
+            result += roman_dict[s[i]]
+    return result
+
+
+def test_function(f):
+    assert f("III") == 3
+    assert f("LVIII") == 58
+
+
+test_function(roman_to_int)
