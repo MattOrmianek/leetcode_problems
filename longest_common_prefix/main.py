d58e14277bdf35821d0a35ea039748a699e2fdaa
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-30 17:24:00 +0200

    feature: added longest common prefix, WIP

---
+++ longest_common_prefix/main.py
@@ -0,0 +1,50 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+"""
+
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    min_len = min_len = min(len(s) for s in strs)
+    common = ""
+    for i in range(0, min_len):
+        candidate = []
+        for string in strs:
+            candidate.append(string[i])
+        if len(set(candidate)) == 1:
+            common += candidate[0]
+        if len(set(candidate)) != 1:
+            break
+
+    return common
+
+
+def test_function(f):
+    assert f(["flower","flow","flight"]) == "fl"
+    assert f(["dog","racecar","car"]) == ""
+    assert f(["cir","car"]) == "c"
+
+test_function(longestCommonPrefix)
