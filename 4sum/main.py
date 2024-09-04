53afeaec0ac4722cb34c2b12f7e17e1a2076b458
Mateusz Ormianek <mattormianek@gmail.com>
2024-09-04 22:57:53 +0200

    refactor: fast version of 4sum

--- 4sum/main.py
+++ 4sum/main.py
@@ -1,5 +1,6 @@
 """
-Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+Given an array nums of n integers, return an array of all the unique quadruplets
+[nums[a], nums[b], nums[c], nums[d]] such that:
 
 0 <= a, b, c, d < n
 a, b, c, and d are distinct.
@@ -25,28 +26,53 @@ Constraints:
 -109 <= target <= 109
 """
 
+# pylint:disable=invalid-name
+
 
 def fourSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    """
     nums.sort()
-    result = []
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            left, right = j + 1, len(nums) - 1
-            while left < right:
-                current_sum = nums[i] + nums[j] + nums[left] + nums[right]
-                if current_sum < target:
-                    left += 1
-                elif current_sum > target:
-                    right -= 1
+    results = []
+    length = len(nums)
+
+    for i in range(length - 3):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        if nums[i] * 4 > target:
+            break
+
+        for j in range(i + 1, length - 2):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            if nums[i] + nums[j] * 3 > target:
+                break
+            l, r = j + 1, length - 1
+            while l < r:
+                total = nums[i] + nums[j] + nums[l] + nums[r]
+
+                if total == target:
+                    results.append([nums[i], nums[j], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif total < target:
+                    l += 1
                 else:
-                    if sorted([nums[i], nums[left], nums[right], nums[j]]) not in result:
-                        result.append(sorted([nums[i], nums[left], nums[right], nums[j]]))
-                    left += 1
-                    right -= 1
-    return result
+                    r -= 1
+    return results
 
 
 def test_function(f):
+    """
+    Testing 4sum
+    """
     assert f([1, 0, -1, 0, -2, 2], 0) == [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
     assert f([2, 2, 2, 2, 2], 8) == [[2, 2, 2, 2]]
 
