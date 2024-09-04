401d2cd571d55d66a90f1d68ef0f8253f7dba3de
Mateusz Ormianek <mattormianek@gmail.com>
2024-09-04 22:54:10 +0200

    feature: added 4sum - version first - slow

---
+++ 4sum/main.py
@@ -0,0 +1,55 @@
+"""
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+
+
+Constraints:
+
+1 <= nums.length <= 200
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+"""
+
+
+def fourSum(nums, target):
+    nums.sort()
+    result = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            left, right = j + 1, len(nums) - 1
+            while left < right:
+                current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    if sorted([nums[i], nums[left], nums[right], nums[j]]) not in result:
+                        result.append(sorted([nums[i], nums[left], nums[right], nums[j]]))
+                    left += 1
+                    right -= 1
+    return result
+
+
+def test_function(f):
+    assert f([1, 0, -1, 0, -2, 2], 0) == [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+    assert f([2, 2, 2, 2, 2], 8) == [[2, 2, 2, 2]]
+
+
+if __name__ == "__main__":
+    test_function(fourSum)
